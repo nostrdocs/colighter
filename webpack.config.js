fcc96e671f63927ad772e26e8bb5e8c07116da39
balogunofafrica <balogunakanbi.k@gmail.com>
2023-03-20 13:03:37 +0100

    chore(config): add style/css loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,46 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env) => {
-  const isProduction = env?.production;
+	const isProduction = env?.production;
 
-  return merge(
-    {
-      entry: {
-        app: "./src/index.tsx",
-      },
-      resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-      },
-      module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-          },
-        ],
-      },
-      output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        library: "[name]",
-        // https://github.com/webpack/webpack/issues/5767
-        // https://github.com/webpack/webpack/issues/7939
-        devtoolNamespace: "colighter",
-        libraryTarget: "umd",
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          process: "process/browser",
-        }),
-        new HtmlWebpackPlugin({
-          template: "./src/index.html",
-        }),
-      ],
-    },
-    isProduction ? require("./webpack.prod") : require("./webpack.dev")
-  );
+	return merge(
+		{
+			entry: {
+				app: "./src/index.tsx",
+			},
+			resolve: {
+				extensions: [".ts", ".tsx", ".js"],
+			},
+			module: {
+				rules: [
+					{
+						test: /\.tsx?$/,
+						loader: "ts-loader",
+					},
+					{
+						test: /\.css$/i,
+						use: ["style-loader", "css-loader"],
+					},
+				],
+			},
+			output: {
+				filename: "[name].bundle.js",
+				path: path.resolve(__dirname, "dist"),
+				library: "[name]",
+				// https://github.com/webpack/webpack/issues/5767
+				// https://github.com/webpack/webpack/issues/7939
+				devtoolNamespace: "colighter",
+				libraryTarget: "umd",
+			},
+			plugins: [
+				new webpack.ProvidePlugin({
+					process: "process/browser",
+				}),
+				new HtmlWebpackPlugin({
+					template: "./src/index.html",
+				}),
+			],
+		},
+		isProduction ? require("./webpack.prod") : require("./webpack.dev"),
+	);
 };
