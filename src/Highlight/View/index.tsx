97b6509a30b6492485294247c3270ef97b9a8291
okjodom <okjodom@gmail.com>
2023-03-21 19:03:42 +0300

    chore reparent style.css and clean up index files

--- src/Highlight/View/index.tsx
+++ src/Highlight/View/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Extension from "./Extension";
-import "./style.css";
+import "./Styles/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-
 root.render(<Extension />);
