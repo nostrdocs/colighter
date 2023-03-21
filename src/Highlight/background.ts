ac5def5a42e9fce158dfe536436fcc2db1de8677
okjodom <okjodom@gmail.com>
2023-03-21 21:29:24 +0300

    nit: prettify background.ts

--- src/Highlight/background.ts
+++ src/Highlight/background.ts
@@ -1,32 +1,36 @@
-chrome.runtime.sendMessage("I am loading content script", (response) => {
-	console.log(response);
-	console.log("I am content script");
-});
+const background = () => {
+	chrome.runtime.sendMessage("I am loading content script", (response) => {
+		console.log(response);
+		console.log("I am content script");
+	});
 
-chrome.contextMenus.create({
-	id: "highlight-selection",
-	title: "Highlight",
-	contexts: ["selection"],
-});
+	chrome.contextMenus.create({
+		id: "highlight-selection",
+		title: "Highlight",
+		contexts: ["selection"],
+	});
 
-chrome.contextMenus.create({
-	id: "remove-selection",
-	title: "Remove Highlight",
-	contexts: ["selection"],
-});
+	chrome.contextMenus.create({
+		id: "remove-selection",
+		title: "Remove Highlight",
+		contexts: ["selection"],
+	});
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
-	const currentTab = tab?.id;
+	chrome.contextMenus.onClicked.addListener(function (info, tab) {
+		const currentTab = tab?.id;
 
-	if (!currentTab) return;
+		if (!currentTab) return;
 
-	if (info.menuItemId === "highlight-selection") {
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(currentTab, { action: "HIGHLIGHT" });
-		});
-	} else if (info.menuItemId === "remove-selection") {
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(currentTab, { action: "REMOVE_HIGHLIGHT" });
-		});
-	}
-});
+		if (info.menuItemId === "highlight-selection") {
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+				chrome.tabs.sendMessage(currentTab, { action: "HIGHLIGHT" });
+			});
+		} else if (info.menuItemId === "remove-selection") {
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+				chrome.tabs.sendMessage(currentTab, { action: "REMOVE_HIGHLIGHT" });
+			});
+		}
+	});
+};
+
+background();
