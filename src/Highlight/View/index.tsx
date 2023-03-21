8003792330fe3402fc502ffbbd4a7b03f4064279
Samson Olusegun <olusegunsamson@live.com>
2023-03-21 10:13:25 +0100

    move templates into highlight/view dir

--- src/Highlight/View/index.tsx
+++ src/Highlight/View/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../Highlight/Components/App";
+import Extension from "./Extension";
 import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-root.render(<App/>);
+root.render(<Extension/>);
