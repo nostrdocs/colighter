6e606522b7d0d2bfdebe5d7886817c4fa33bd19b
okjodom <okjodom@gmail.com>
2023-03-22 15:32:41 +0300

    fix: memoize Extension to avoid rerenders?

--- src/Highlight/View/index.tsx
+++ src/Highlight/View/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Extension from "./Extension";
+import { Extension } from "./Extension";
 import "./Styles/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
