e72e0b27badc41e71bb921eba2dc7c85afbff536
okjodom <okjodom@gmail.com>
2023-03-23 16:07:05 +0300

    chore: revert use of custom base asset tracking

--- src/Highlight/model.ts
+++ src/Highlight/model.ts
@@ -2,9 +2,6 @@ import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
 import { IHighlight, IHighlightCollection } from "./types";
 import { sha256Hash } from "./utils";
 
-const HIGHLIGHT_BASE_ASSET_KEY = "HIGHLIGHT_BASE_ASSET_KEY";
-const HIGHLIGHT_BASE_ASSET_DEFAULT_VALUE = "HIGHLIGHT_BASE_ASSET_DEFAULT_VALUE";
-
 export class Highlight implements IHighlight {
 	protected constructor(public author: string, public text: string, public hashId: string) {}
 
@@ -15,24 +12,10 @@ export class Highlight implements IHighlight {
 }
 
 export class HighlightCollection extends DataObject implements IHighlightCollection {
-	protected async initializeFirstTime() {
-		// Store  a reference to the web resource we are highlightning, as a sha256 hash of the web url
-		// In case we cannot determine the web url at the time of initialization,
-		// we use a hash of HIGHLIGHT_BASE_ASSET_DEFAULT_VALUE as the base asset reference
-		const base = await sha256Hash(document.location.href || HIGHLIGHT_BASE_ASSET_DEFAULT_VALUE);
-		this.root.set(HIGHLIGHT_BASE_ASSET_KEY, base);
-	}
+	protected async initializeFirstTime() {}
 
 	protected async hasInitialized() {
 		this.root.on("valueChanged", () => this.emit("highlightCollectionChanged"));
-	}
-
-	async getHighlightBase(): Promise<string> {
-		let base =
-			this.root.get(HIGHLIGHT_BASE_ASSET_KEY) ||
-			sha256Hash(HIGHLIGHT_BASE_ASSET_DEFAULT_VALUE);
-
-		return Promise.resolve(base);
 	}
 
 	async addHighlight(highlight: Highlight): Promise<Highlight> {
