a0be13c33c5ff1fe01f2dbd6f575067befcee06a
okjodom <okjodom@gmail.com>
2023-03-24 01:06:30 +0300

    fix: background!

--- src/Highlight/background.ts
+++ src/Highlight/background.ts
@@ -1,42 +1,44 @@
 import { MessageAction } from "./types";
 
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.contextMenus.create(
-		{
-			id: "colighter-add",
-			title: "Highlight",
-			contexts: ["selection"],
-		},
-		() => {
-			chrome.tabs.query({ active: true, currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-				chrome.tabs
-					.sendMessage(tabs[0].id!, { action: MessageAction.RENDER_HIGHLIGHTS })
-					.catch((err) => {
-						console.log(err);
-					});
-			});
-		},
-	);
+	chrome.contextMenus.create({
+		id: "create-highlight",
+		title: "Create Highlight",
+		contexts: ["selection"],
+	});
 
-	chrome.contextMenus.create(
-		{
-			id: "colighter-remove",
-			title: "Remove Highlight",
-			contexts: ["selection"],
-		},
-		() => {
-			chrome.tabs.query({ active: true, currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-				chrome.tabs
-					.sendMessage(tabs[0].id!, { action: MessageAction.REMOVE_HIGHLIGHTS })
-					.catch((err) => {
-						console.log(err);
-					});
-			});
-		},
-	);
+	chrome.contextMenus.create({
+		id: "remove-highlight",
+		title: "Remove Highlight",
+		contexts: ["selection"],
+	});
+
+	chrome.contextMenus.onClicked.addListener((info, tab) => {
+		let tabId = tab?.id;
+		if (tabId === undefined) return;
+
+		if (info.menuItemId === "create-highlight") {
+			chrome.tabs
+				.sendMessage(tabId, { action: MessageAction.RENDER_HIGHLIGHTS })
+				.catch((err) => {
+					console.log(err);
+				});
+
+			return;
+		}
+
+		if (info.menuItemId === "remove-highlight") {
+			chrome.tabs
+				.sendMessage(tabId, { action: MessageAction.REMOVE_HIGHLIGHTS })
+				.catch((err) => {
+					console.log(err);
+				});
+			return;
+		}
+	});
 
 	chrome.commands.onCommand.addListener((command, tab) => {
-		if (command === "execute-highlight") {
+		if (command === "create-highlight") {
 			tab.id !== undefined &&
 				chrome.tabs
 					.sendMessage(tab.id, { action: MessageAction.RENDER_HIGHLIGHTS })
