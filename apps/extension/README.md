64664fe43dc4b712db7d49cfba264631580ed1f8
okjodom <okjodom@gmail.com>
2023-05-21 14:56:20 -0700

    fix(extension): bundle an installable extension

--- apps/extension/README.md
+++ apps/extension/README.md
@@ -7,11 +7,9 @@ Browser extension for colighter, currently only supports chromium browsers
 Navigate to the root of this extension app, then run
 
 - `yarn` to install dependencies
-- `yarn build` to build the project
-- configure `COLLAB_RELAY_URL` in **.env** file as necessary
-- install the resulting build output in a chromium browser and test
+- `yarn dev` to build the project. This builds the extension and runs a development collab relay to test against
+- install the resulting build outputs in a chromium browser and test the experience
 
-> Note: If `COLLAB_RELAY_URL` is configured to `http://localhost:7070`,
-> you should `run npm start:server` in a second shell
+To build a version of the extension optimized for production, run `yarn build`
 
 You can also refer to the [Development](../../README.md#development) guide at the root of the project
---
+++ apps/extension/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  const isProduction = env?.production;
+
+  return merge(
+    {
+      entry: {
+        extension: './src/view/index.tsx',
+        background: './src/ext_background.ts',
+        contentscript: './src/ext_contentscript.ts',
+      },
+      resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.svg$/,
+            use: [
+              {
+                loader: 'svg-url-loader',
+                options: {
+                  limit: 10000,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        library: '[name]',
+        // https://github.com/webpack/webpack/issues/5767
+        // https://github.com/webpack/webpack/issues/7939
+        devtoolNamespace: 'colighter',
+        libraryTarget: 'umd',
+        publicPath: '',
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+        }),
+      ],
+    },
+    isProduction ? require('./webpack.prod') : require('./webpack.dev')
+  );
+};
