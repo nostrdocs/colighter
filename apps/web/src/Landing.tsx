e0f591137f3072b538b09dfe1122037940419d85
balogunofafrica <balogunakanbi.k@gmail.com>
2023-05-27 20:45:48 +0100

    update landing page

--- apps/web/src/Landing.tsx
+++ apps/web/src/Landing.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Hero from './assets/colighter.avif';
 import { ReactComponent as BookingSvg } from './assets/booking.svg';
+import { ReactComponent as CommunitySvg } from './assets/community.svg';
 import { ReactComponent as WorldSvg } from './assets/world.svg';
 import { ReactComponent as WaveOneSvg } from './assets/waveone.svg';
 import { ReactComponent as WaveTwoSvg } from './assets/wavetwo.svg';
@@ -59,66 +60,82 @@ export const Landing: React.FC = () => {
       <Header />
       {/* Hero */}
       <div className='pt-24'>
-        <div className='container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center'>
-          <div className='flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left'>
-            <p className='uppercase tracking-loose w-full'>Coming soon!</p>
+        <div className='container flex flex-col flex-wrap items-center px-3 mx-auto md:flex-row'>
+          <div className='flex flex-col items-start justify-center w-full text-center md:w-2/5 md:text-left'>
             <h1 className='my-4 text-4xl font-bold leading-tight'>
-              Highlight, Capture, and Organize Your Digital World
+              Highlight, share, and discuss online text over nostr
             </h1>
-            <p className='leading-normal text-2xl mb-8'>
+            <p className='mb-8 text-2xl leading-normal'>
               Enhance productivity with effortless highlighting. No more manual
               struggles or messy annotations. Simplify emphasizing and
               remembering crucial content.
             </p>
             <WaitlistButton />
           </div>
-
-          <div className='w-full md:w-3/5 py-6 text-center'>
-            <img className='w-full md:w-4/5 z-50' src={Hero} />
+          <div className='flex justify-end w-full text-center md:w-3/5'>
+            <img
+              alt='Colighter-Hero'
+              className='z-50 w-full md:w-4/5'
+              src={Hero}
+            />
           </div>
         </div>
       </div>
 
-      <div className='relative -mt-12 lg:-mt-24'>
+      <div className='-mt-12 lg:-mt-24'>
         <WaveOneSvg />
       </div>
 
-      <section className='bg-white py-8'>
-        <div className='container max-w-5xl mx-auto m-8'>
+      <section className='py-8 bg-white'>
+        <div className='container max-w-5xl m-8 mx-auto'>
           <h2 className='w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800'>
             Features
           </h2>
           <div className='w-full mb-4'>
-            <div className='h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t'></div>
+            <div className='w-64 h-1 py-0 mx-auto my-0 rounded-t opacity-25 gradient'></div>
           </div>
-          <div className='flex flex-wrap'>
-            <div className='w-5/6 sm:w-1/2 p-6'>
-              <h3 className='text-3xl text-gray-800 font-bold leading-none mb-3'>
+          <div className='flex flex-wrap items-center'>
+            <div className='flex flex-col w-5/6 gap-3 p-6 sm:w-1/2'>
+              <h3 className='text-3xl font-bold leading-none text-gray-800'>
                 Effortless Highlighting
               </h3>
-              <p className='text-gray-600 mb-8'>
+              <p className='text-gray-600'>
                 Instantly mark text in vibrant colors with a single click,
                 creating a visual roadmap of your digital discoveries.
               </p>
             </div>
-            <div className='w-full sm:w-1/2 p-6'>
+            <div className='w-full p-6 sm:w-1/2'>
               <BookingSvg />
             </div>
           </div>
-          <div className='flex flex-wrap flex-col-reverse sm:flex-row'>
-            <div className='w-full sm:w-1/2 p-6 mt-6'>
+          <div className='flex flex-col-reverse flex-wrap items-center sm:flex-row'>
+            <div className='w-full p-6 mt-6 sm:w-1/2'>
               <WorldSvg />
             </div>
-            <div className='w-full sm:w-1/2 p-6 mt-6'>
-              <div className='align-middle'>
-                <h3 className='text-3xl text-gray-800 font-bold leading-none mb-3'>
+            <div className='w-full p-6 mt-6 sm:w-1/2'>
+              <div className='flex flex-col gap-3 align-middle'>
+                <h3 className='text-3xl font-bold leading-none text-gray-800 '>
                   Share Highlights With The World
                 </h3>
-                <p className='text-gray-600 mb-8'>
+                <p className='text-gray-600 '>
                   Share your highlights effortlessly with the world, amplifying
                   your impact and sparking engaging discussions.
                 </p>
               </div>
+            </div>
+          </div>
+          <div className='flex flex-wrap items-center'>
+            <div className='flex flex-col w-5/6 gap-3 p-6 sm:w-1/2'>
+              <h3 className='text-3xl font-bold leading-none text-gray-800'>
+                Effortless Highlighting
+              </h3>
+              <p className='text-gray-600'>
+                Instantly mark text in vibrant colors with a single click,
+                creating a visual roadmap of your digital discoveries.
+              </p>
+            </div>
+            <div className='w-full p-6 sm:w-1/2'>
+              <CommunitySvg />
             </div>
           </div>
         </div>
@@ -127,12 +144,12 @@ export const Landing: React.FC = () => {
       {/* <!-- Change the colour #f8fafc to match the previous section colour --> */}
       <WaveTwoSvg />
 
-      <section className='container mx-auto text-center py-6 mb-12'>
+      <section className='container p-6 pb-12 mx-auto text-center'>
         <h2 className='w-full my-2 text-5xl font-bold leading-tight text-center text-white'>
           Experience Colighter
         </h2>
         <div className='w-full mb-4'>
-          <div className='h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t'></div>
+          <div className='w-1/6 h-1 py-0 mx-auto my-0 bg-white rounded-t opacity-25'></div>
         </div>
         <p className='my-4 text-3xl leading-tight'>
           Elevate your reading with our text highlighter extension. Highlight,
@@ -141,130 +158,6 @@ export const Landing: React.FC = () => {
         </p>
         <WaitlistButton />
       </section>
-
-      {/* <!--Footer--> */}
-      <footer className='bg-white'>
-        <div className='container mx-auto px-8'>
-          <div className='w-full flex flex-col md:flex-row py-6'>
-            <div className='flex-1 mb-6 text-black'>
-              <a
-                className='text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl'
-                href='#'
-              >
-                COLIGHTER
-              </a>
-            </div>
-            <div className='flex-1'>
-              <p className='uppercase text-gray-500 md:mb-6'>Links</p>
-              <ul className='list-reset mb-6'>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    FAQ
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Help
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Support
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className='flex-1'>
-              <p className='uppercase text-gray-500 md:mb-6'>Legal</p>
-              <ul className='list-reset mb-6'>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Terms
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Privacy
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className='flex-1'>
-              <p className='uppercase text-gray-500 md:mb-6'>Social</p>
-              <ul className='list-reset mb-6'>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Facebook
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Linkedin
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Twitter
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className='flex-1'>
-              <p className='uppercase text-gray-500 md:mb-6'>Company</p>
-              <ul className='list-reset mb-6'>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Official Blog
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li className='mt-2 inline-block mr-2 md:block md:mr-0'>
-                  <a
-                    href='#'
-                    className='no-underline hover:underline text-gray-800 hover:text-pink-500'
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 };
