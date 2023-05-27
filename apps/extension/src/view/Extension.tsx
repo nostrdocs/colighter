0733f7e5d249df098d5a06a76031ac02949f79d3
okjodom <okjodom@gmail.com>
2023-05-27 15:14:46 -0700

    fix(ext): fix extension bundling

--- apps/extension/src/view/Extension.tsx
+++ apps/extension/src/view/Extension.tsx
@@ -1,9 +1,3 @@
-import '@fontsource/inter/400.css';
-import '@fontsource/oxygen-mono/400.css';
-import '@fontsource/quicksand/400.css';
-import '@fontsource/quicksand/500.css';
-import '@fontsource/quicksand/600.css';
-
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { HomePopUp } from './components';
