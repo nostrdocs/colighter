bb8d620f72b44512d3a1133c5360c97b58645d9b
Theophilus <isahtheophilus@gmail.com>
2023-06-20 12:12:40 +0100

    feat: add nip07

--- apps/extension/src/components/Popup.tsx
+++ apps/extension/src/components/Popup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Box, Button, Flex, Heading, Image } from '@chakra-ui/react';
 
 import Colighter from '../assets/colighter.svg';
@@ -7,9 +7,30 @@ import { useSidebar } from '../context/context';
 import { theme } from '../theme';
 import { openSidebar } from '../utils/Event';
 import { Highlights } from './Highlights';
+import { PartialKeyPair, browserSourceNostrId } from 'nostrfn';
 
 export function Popup() {
   const { toggleSidebar } = useSidebar();
+  const [nostrId, setNostrId] = useState<PartialKeyPair | null>(null);
+  const [showCreateButton, setShowCreateButton] = useState<boolean>(true);
+
+  const handleCreateNostrId = useCallback(async () => {
+    const newNostrId = await browserSourceNostrId();
+    setNostrId(newNostrId);
+    localStorage.setItem('nostrKeys', JSON.stringify(newNostrId));
+    setShowCreateButton(false);
+    // console.log(newNostrId);
+  }, []);
+
+  useEffect(() => {
+    const storedNostrKeys = localStorage.getItem('nostrKeys');
+    if (storedNostrKeys) {
+      const parsedKeys = JSON.parse(storedNostrKeys);
+      setNostrId(parsedKeys);
+      setShowCreateButton(false);
+      // console.log(parsedKeys);
+    }
+  }, []);
   return (
     <Flex
       overflow='hidden'
@@ -23,8 +44,12 @@ export function Popup() {
       <Flex justifyContent='space-between'>
         <Image src={`${Colighter}`} alt='colighter-logo' />
         <Image src={`${Gear}`} alt='gear-icon' />
+        {showCreateButton ? (
+          <button onClick={handleCreateNostrId}>Create NostrId</button>
+        ) : null}
       </Flex>
       <Box>
+        <div>Your npub is: {nostrId ? nostrId.pubkey.toString() : 'N/A'}</div>
         <Heading as='h5' fontWeight='500' m={0} mb='12px'>
           Your Highlights
         </Heading>
