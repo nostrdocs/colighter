4fdb11bd94d11076e669b3ab4fa960b04b55e3a8
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-06-26 15:21:50 +0100

    chore: debugged delay in sidebar

--- apps/extension/src/sidebar.tsx
+++ apps/extension/src/sidebar.tsx
@@ -13,6 +13,7 @@ const SidebarApp = () => {
   React.useEffect(() => {
     function handleMessage(message: { action: MessageAction }) {
       if (message.action === MessageAction.OPEN_SIDEBAR) {
+        console.log("Testing if side bar is open")
         toggleSidebar();
       }
       if (message.action === MessageAction.CLOSE_SIDEBAR) {
