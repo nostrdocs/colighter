8ad1b9bdda97ab976c5fd06b6c7f13d5160c0515
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-06-27 19:51:12 +0100

    feat: added created_at and user pubkey to highlights

--- apps/extension/src/components/Highlights.tsx
+++ apps/extension/src/components/Highlights.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Avatar,  Box, Flex, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
 import { useNostrHighlights } from '../hooks/useNostrHighlights';
 import { IHighlight } from '../types';
 import { theme } from '../theme';
 import { convertUnixTimestampToDate } from '../utils/unixConverter';
+import { useSettings } from '../context/settingsContext';
 
 type HighlightsProps = {
   showRecentOnly?: boolean;
@@ -13,7 +14,7 @@ type HighlightsProps = {
 export function Highlights({ showRecentOnly }: HighlightsProps) {
   const [highlights] = useNostrHighlights();
   const highlightsToShow = showRecentOnly ? highlights.slice(0, 1) : highlights;
-  console.log(highlights,"hightds showing")
+  console.log(highlights, 'hightds showing');
   return (
     <>
       {highlightsToShow.map((highlight) => (
@@ -23,10 +24,34 @@ export function Highlights({ showRecentOnly }: HighlightsProps) {
   );
 }
 
-type HighlightViewProps = Pick<IHighlight, 'text' | 'author' | 'id' | "created_at">;
+type HighlightViewProps = Pick<
+  IHighlight,
+  'text' | 'author' | 'id' | 'created_at'
+>;
 
-const HighlightView = ({ text, id, author, created_at  }: HighlightViewProps) => {
-  // convertUnixTimestampToDate()
+const HighlightView = ({
+  text,
+  id,
+  author,
+  created_at,
+}: HighlightViewProps) => {
+  const { settings } = useSettings();
+  const [nostrId, setNostrId] = useState<{
+    privkey: string;
+    pubkey: string;
+  } | null>(null);
+
+  useEffect(() => {
+    settings
+      .getNostrIdentity()
+      .then((id) => {
+        setNostrId(id);
+      })
+      .catch((err) => {
+        // Show error message
+        console.log(err);
+      });
+  }, [settings]);
   return (
     <Box
       key={id}
@@ -37,9 +62,22 @@ const HighlightView = ({ text, id, author, created_at  }: HighlightViewProps) =>
       <Text textAlign='left' padding='12px' fontStyle='italic'>
         {text}
       </Text>
-      <Flex padding={"12px"} justifyContent={"space-between"} alignItems={"center"}>
-        {author && <Avatar name={author} width={25} height={25}/>}
-        {created_at && <Text color={theme.palette.secondaryTint} fontWeight={700}>{convertUnixTimestampToDate(created_at)}</Text>}
+      <Flex
+        padding={'12px'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+      >
+        {/* i don't know if this is the best way to handle it  
+           most times the author is an empty string?
+        */}
+        {(author || nostrId?.pubkey) && (
+          <Avatar name={author || nostrId?.pubkey} width={25} height={25} />
+        )}
+        {created_at && (
+          <Text color={theme.palette.secondaryTint} fontWeight={700}>
+            {convertUnixTimestampToDate(created_at)}
+          </Text>
+        )}
       </Flex>
     </Box>
   );
